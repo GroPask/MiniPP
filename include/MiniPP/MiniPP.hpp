13b7a0e1b280db3343b8e395b2535d0b563d0646
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-12-03 22:03:54 +0100

    Add basic feature

---
+++ include/MiniPP/MiniPP.hpp
@@ -0,0 +1,49 @@
+#ifndef MINI_PP_HPP
+#define MINI_PP_HPP
+
+////////////////////////////////////////////////////////////
+// Base
+////////////////////////////////////////////////////////////
+
+#define MINI_PP_PARENS ()
+
+#define MINI_PP_CAT(a, b) MINI_PP_PRIVATE_CAT_IMPL(a, b)
+#define MINI_PP_PRIVATE_CAT_IMPL(a, b) a ## b
+
+#define MINI_PP_TO_TEXT(x) MINI_PP_PRIVATE_TO_TEXT_IMPL(x)
+#define MINI_PP_PRIVATE_TO_TEXT_IMPL(x) #x
+
+#define MINI_PP_DEFER(x) x MINI_PP_PRIVATE_DEFER_EMPTY()
+#define MINI_PP_PRIVATE_DEFER_EMPTY()
+
+#define MINI_PP_EXPAND(x) x
+
+////////////////////////////////////////////////////////////
+// Boolean
+////////////////////////////////////////////////////////////
+
+#define MINI_PP_NOT(cond01) MINI_PP_CAT(MINI_PP_PRIVATE_NOT_SWITCH_, cond01)
+#define MINI_PP_PRIVATE_NOT_SWITCH_0 1
+#define MINI_PP_PRIVATE_NOT_SWITCH_1 0
+
+#define MINI_PP_OR(cond01A, cond01B) MINI_PP_CAT(MINI_PP_CAT(MINI_PP_PRIVATE_OR_SWITCH_, cond01A), cond01B)
+#define MINI_PP_PRIVATE_OR_SWITCH_00 0
+#define MINI_PP_PRIVATE_OR_SWITCH_01 1
+#define MINI_PP_PRIVATE_OR_SWITCH_10 1
+#define MINI_PP_PRIVATE_OR_SWITCH_11 1
+
+#define MINI_PP_AND(cond01A, cond01B) MINI_PP_CAT(MINI_PP_CAT(MINI_PP_PRIVATE_AND_SWITCH_, cond01A), cond01B)
+#define MINI_PP_PRIVATE_AND_SWITCH_00 0
+#define MINI_PP_PRIVATE_AND_SWITCH_01 0
+#define MINI_PP_PRIVATE_AND_SWITCH_10 0
+#define MINI_PP_PRIVATE_AND_SWITCH_11 1
+
+#define MINI_PP_IF(cond01, x) MINI_PP_CAT(MINI_PP_PRIVATE_IF_SWITCH_, cond01)(x)
+#define MINI_PP_PRIVATE_IF_SWITCH_0(x)
+#define MINI_PP_PRIVATE_IF_SWITCH_1(x) x
+
+#define MINI_PP_IF_ELSE(cond01, a, b) MINI_PP_CAT(MINI_PP_PRIVATE_IF_ELSE_SWITCH_, cond01)(a, b)
+#define MINI_PP_PRIVATE_IF_ELSE_SWITCH_0(a, b) b
+#define MINI_PP_PRIVATE_IF_ELSE_SWITCH_1(a, b) a
+
+#endif
---
+++ tests/Boolean.cpp
@@ -0,0 +1,42 @@
+#include "MiniPP/MiniPP.hpp"
+
+#define ZERO 0
+#define ONE() 1
+
+///////////////// MINI_PP_NOT /////////////////
+static_assert(MINI_PP_NOT(0) == 1, "MINI_PP_NOT is broken");
+static_assert(MINI_PP_NOT(1) == 0, "MINI_PP_NOT is broken");
+static_assert(MINI_PP_NOT(ZERO) == 1, "MINI_PP_NOT is broken");
+static_assert(MINI_PP_NOT(ONE()) == 0, "MINI_PP_NOT is broken");
+
+///////////////// MINI_PP_OR /////////////////
+static_assert(MINI_PP_OR(0, 0) == 0, "MINI_PP_OR is broken");
+static_assert(MINI_PP_OR(0, 1) == 1, "MINI_PP_OR is broken");
+static_assert(MINI_PP_OR(1, 0) == 1, "MINI_PP_OR is broken");
+static_assert(MINI_PP_OR(1, 1) == 1, "MINI_PP_OR is broken");
+static_assert(MINI_PP_OR(ZERO, ZERO) == 0, "MINI_PP_OR is broken");
+static_assert(MINI_PP_OR(ZERO, ONE()) == 1, "MINI_PP_OR is broken");
+static_assert(MINI_PP_OR(ONE(), ZERO) == 1, "MINI_PP_OR is broken");
+static_assert(MINI_PP_OR(ONE(), ONE()) == 1, "MINI_PP_OR is broken");
+
+///////////////// MINI_PP_AND /////////////////
+static_assert(MINI_PP_AND(0, 0) == 0, "MINI_PP_AND is broken");
+static_assert(MINI_PP_AND(0, 1) == 0, "MINI_PP_AND is broken");
+static_assert(MINI_PP_AND(1, 0) == 0, "MINI_PP_AND is broken");
+static_assert(MINI_PP_AND(1, 1) == 1, "MINI_PP_AND is broken");
+static_assert(MINI_PP_AND(ZERO, ZERO) == 0, "MINI_PP_AND is broken");
+static_assert(MINI_PP_AND(ZERO, ONE()) == 0, "MINI_PP_AND is broken");
+static_assert(MINI_PP_AND(ONE(), ZERO) == 0, "MINI_PP_AND is broken");
+static_assert(MINI_PP_AND(ONE(), ONE()) == ONE(), "MINI_PP_AND is broken");
+
+///////////////// MINI_PP_IF /////////////////
+static_assert(MINI_PP_IF(0, 4) 2 == 2, "MINI_PP_IF is broken");
+static_assert(MINI_PP_IF(1, 42) == 42, "MINI_PP_IF is broken");
+static_assert(MINI_PP_IF(ZERO, 4) 2 == 2, "MINI_PP_IF is broken");
+static_assert(MINI_PP_IF(ONE(), 42) == 42, "MINI_PP_IF is broken");
+
+///////////////// MINI_PP_IF_ELSE /////////////////
+static_assert(MINI_PP_IF_ELSE(0, 123, 456) == 456, "MINI_PP_IF_ELSE is broken");
+static_assert(MINI_PP_IF_ELSE(1, 123, 456) == 123, "MINI_PP_IF_ELSE is broken");
+static_assert(MINI_PP_IF_ELSE(ZERO, 123, 456) == 456, "MINI_PP_IF_ELSE is broken");
+static_assert(MINI_PP_IF_ELSE(ONE(), 123, 456) == 123, "MINI_PP_IF_ELSE is broken");
