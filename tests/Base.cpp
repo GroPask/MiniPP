c10e4f4ca741cbe36d797d233c4c405a36d651be
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-12-05 09:07:03 +0100

    Add MINI_PP_CALL_WITH_ARGS

--- tests/Base.cpp
+++ tests/Base.cpp
@@ -6,6 +6,9 @@
 #define VALUE_2 2
 #define VALUE_42() 42
 #define BEGIN VALU
+#define ARG_SEQ_4_2 4, 2
+#define GET_FIRST(a, b) a
+#define GET_SECOND(a, b) b
 
 ///////////////// MINI_PP_CAT /////////////////
 static_assert(MINI_PP_CAT(4, 2) == 42, "MINI_PP_CAT is broken");
@@ -43,6 +46,15 @@ static_assert(MINI_PP_EXPAND(VALUE_4) == 4, "MINI_PP_EXPAND is broken");
 static_assert(MINI_PP_EXPAND(VALUE_42()) == 42, "MINI_PP_EXPAND is broken");
 static_assert(MINI_PP_EXPAND(VALUE_42 MINI_PP_PARENS) == 42, "MINI_PP_EXPAND is broken");
 
+///////////////// MINI_PP_CALL_WITH_ARGS /////////////////
+static_assert(MINI_PP_CALL_WITH_ARGS(VALUE_42) == 42, "MINI_PP_CALL_WITH_ARGS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(GET_FIRST, 4, 2) == 4, "MINI_PP_CALL_WITH_ARGS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(GET_SECOND, 4, 2) == 2, "MINI_PP_CALL_WITH_ARGS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(GET_FIRST, VALUE_4, 2) == 4, "MINI_PP_CALL_WITH_ARGS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(GET_SECOND, VALUE_4, VALUE_2) == 2, "MINI_PP_CALL_WITH_ARGS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(GET_FIRST, ARG_SEQ_4_2) == 4, "MINI_PP_CALL_WITH_ARGS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(GET_SECOND, ARG_SEQ_4_2) == 2, "MINI_PP_CALL_WITH_ARGS is broken");
+
 ///////////////// MINI_PP_TO_TEXT + MINI_PP_CAT /////////////////
 static_assert(MINI_PP_TO_TEXT(MINI_PP_CAT(4, 2))[0] == '4', "MINI_PP_TO_TEXT + MINI_PP_CAT is broken");
 static_assert(MINI_PP_TO_TEXT(MINI_PP_CAT(4, 2))[1] == '2', "MINI_PP_TO_TEXT + MINI_PP_CAT is broken");
@@ -63,3 +75,18 @@ static_assert(MINI_PP_TO_TEXT(MINI_PP_CAT(BEGIN, E_4))[1] == '\0', "MINI_PP_TO_T
 static_assert(MINI_PP_EXPAND(MINI_PP_DEFER(MINI_PP_TO_TEXT)(VALUE_42 MINI_PP_PARENS))[0] == '4', "MINI_PP_EXPAND + MINI_PP_DEFER + MINI_PP_TO_TEXT + MINI_PP_PARENS is broken");
 static_assert(MINI_PP_EXPAND(MINI_PP_DEFER(MINI_PP_TO_TEXT)(VALUE_42 MINI_PP_PARENS))[1] == '2', "MINI_PP_EXPAND + MINI_PP_DEFER + MINI_PP_TO_TEXT + MINI_PP_PARENS is broken");
 static_assert(MINI_PP_EXPAND(MINI_PP_DEFER(MINI_PP_TO_TEXT)(VALUE_42 MINI_PP_PARENS))[2] == '\0', "MINI_PP_EXPAND + MINI_PP_DEFER + MINI_PP_TO_TEXT + MINI_PP_PARENS is broken");
+
+///////////////// MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT /////////////////
+static_assert(MINI_PP_CALL_WITH_ARGS(MINI_PP_TO_TEXT, VALUE_42())[0] == '4', "MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(MINI_PP_TO_TEXT, VALUE_42())[1] == '2', "MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(MINI_PP_TO_TEXT, VALUE_42())[2] == '\0', "MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT is broken");
+
+///////////////// MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT + MINI_PP_PARENS /////////////////
+static_assert(MINI_PP_CALL_WITH_ARGS(MINI_PP_TO_TEXT, VALUE_42 MINI_PP_PARENS)[0] == '4', "MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT + MINI_PP_PARENS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(MINI_PP_TO_TEXT, VALUE_42 MINI_PP_PARENS)[1] == '2', "MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT + MINI_PP_PARENS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(MINI_PP_TO_TEXT, VALUE_42 MINI_PP_PARENS)[2] == '\0', "MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT + MINI_PP_PARENS is broken");
+
+///////////////// MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT + MINI_PP_CALL_WITH_ARGS /////////////////
+static_assert(MINI_PP_CALL_WITH_ARGS(MINI_PP_TO_TEXT, MINI_PP_CALL_WITH_ARGS(VALUE_42))[0] == '4', "MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT + MINI_PP_CALL_WITH_ARGS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(MINI_PP_TO_TEXT, MINI_PP_CALL_WITH_ARGS(VALUE_42))[1] == '2', "MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT + MINI_PP_CALL_WITH_ARGS is broken");
+static_assert(MINI_PP_CALL_WITH_ARGS(MINI_PP_TO_TEXT, MINI_PP_CALL_WITH_ARGS(VALUE_42))[2] == '\0', "MINI_PP_CALL_WITH_ARGS + MINI_PP_TO_TEXT + MINI_PP_CALL_WITH_ARGS is broken");
