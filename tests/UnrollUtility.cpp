77d1b7e5864215de6b4836f014f1153ac1e8c981
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-12-06 09:00:58 +0100

    Foreach wip

--- tests/UnrollUtility.cpp
+++ tests/UnrollUtility.cpp
@@ -78,3 +78,48 @@ static_assert(ArgSeqMaxTo0::doCheck(MINI_PP_PRIVATE_ARG_SEQ_MAX_TO_0), "MINI_PP_
 ///////////////// MINI_PP_PRIVATE_GET_ARG_INDEX_MAX /////////////////
 static_assert(MINI_PP_EXPAND(MINI_PP_DEFER(MINI_PP_PRIVATE_GET_ARG_INDEX_MAX)(MINI_PP_PRIVATE_ARG_SEQ_MAX_MINUS_ONE_TRUE_THEN_FALSE, 42, dummy)) == 42, "MINI_PP_PRIVATE_GET_ARG_INDEX_MAX is broken");
 static_assert(MINI_PP_EXPAND(MINI_PP_DEFER(MINI_PP_PRIVATE_GET_ARG_INDEX_MAX)(MINI_PP_PRIVATE_ARG_SEQ_MAX_TO_0, dummy)) == 0, "MINI_PP_PRIVATE_GET_ARG_INDEX_MAX is broken");
+
+
+
+
+
+#define FOR_EACH_WITH_ARGS2(func, args, ...) MINI_PP_EXPAND(MINI_PP_CAT(DO_FOR_EACH_WITH_ARGS_IMPL_, MINI_PP_NB_ARGS(__VA_ARGS__))(func, args, __VA_ARGS__))
+
+#define DO_FOR_EACH_WITH_ARGS_IMPL_0( func, args, ...)
+#define DO_FOR_EACH_WITH_ARGS_IMPL_1( func, args, first)      func(args, first)
+#define DO_FOR_EACH_WITH_ARGS_IMPL_2( func, args, first, ...) func(args, first) MINI_PP_EXPAND(DO_FOR_EACH_WITH_ARGS_IMPL_1( func, args, __VA_ARGS__))
+#define DO_FOR_EACH_WITH_ARGS_IMPL_3( func, args, first, ...) func(args, first) MINI_PP_EXPAND(DO_FOR_EACH_WITH_ARGS_IMPL_2( func, args, __VA_ARGS__))
+#define DO_FOR_EACH_WITH_ARGS_IMPL_4( func, args, first, ...) func(args, first) MINI_PP_EXPAND(DO_FOR_EACH_WITH_ARGS_IMPL_3( func, args, __VA_ARGS__))
+#define DO_FOR_EACH_WITH_ARGS_IMPL_5( func, args, first, ...) func(args, first) MINI_PP_EXPAND(DO_FOR_EACH_WITH_ARGS_IMPL_4( func, args, __VA_ARGS__))
+#define DO_FOR_EACH_WITH_ARGS_IMPL_6( func, args, first, ...) func(args, first) MINI_PP_EXPAND(DO_FOR_EACH_WITH_ARGS_IMPL_5( func, args, __VA_ARGS__))
+#define DO_FOR_EACH_WITH_ARGS_IMPL_7( func, args, first, ...) func(args, first) MINI_PP_EXPAND(DO_FOR_EACH_WITH_ARGS_IMPL_6( func, args, __VA_ARGS__))
+
+
+#define FOR_EACH_WITH_ARGS(func, args, ...) MINI_PP_CAT(REPEAT_IMPL_, MINI_PP_NB_ARGS(__VA_ARGS__))(FOR_EACH_WITH_ARGS_IMPL, MINI_PP_MAKE_TUPLE(func, args), __VA_ARGS__)
+#define FOR_EACH_WITH_ARGS_IMPL(tupleFuncArgs, first, ...) MINI_PP_TUPLE_GET_ELEM_0(tupleFuncArgs)
+
+#define REPEAT_IMPL_0( func)
+#define REPEAT_IMPL_1( func, ...) MINI_PP_CALL_WITH_ARGS(func, MINI_PP_TUPLE_GET_ELEM_0(MINI_PP_MAKE_TUPLE(__VA_ARGS__)))
+#define REPEAT_IMPL_2( func, ...) MINI_PP_CALL_WITH_ARGS(func, MINI_PP_TUPLE_GET_ELEM_0(MINI_PP_MAKE_TUPLE(__VA_ARGS__))) REPEAT_IMPL_1 MINI_PP_TUPLE_POP_FRONT(MINI_PP_MAKE_TUPLE(__VA_ARGS__))
+#define REPEAT_IMPL_3( func, ...) MINI_PP_CALL_WITH_ARGS(func, MINI_PP_TUPLE_GET_ELEM_0(MINI_PP_MAKE_TUPLE(__VA_ARGS__))) REPEAT_IMPL_2 MINI_PP_TUPLE_POP_FRONT(MINI_PP_MAKE_TUPLE(__VA_ARGS__))
+#define REPEAT_IMPL_4( func, ...) MINI_PP_CALL_WITH_ARGS(func, MINI_PP_TUPLE_GET_ELEM_0(MINI_PP_MAKE_TUPLE(__VA_ARGS__))) REPEAT_IMPL_3 MINI_PP_TUPLE_POP_FRONT(MINI_PP_MAKE_TUPLE(__VA_ARGS__))
+#define REPEAT_IMPL_5( func, ...) MINI_PP_CALL_WITH_ARGS(func, MINI_PP_TUPLE_GET_ELEM_0(MINI_PP_MAKE_TUPLE(__VA_ARGS__))) REPEAT_IMPL_4 MINI_PP_TUPLE_POP_FRONT(MINI_PP_MAKE_TUPLE(__VA_ARGS__))
+#define REPEAT_IMPL_6( func, ...) MINI_PP_CALL_WITH_ARGS(func, MINI_PP_TUPLE_GET_ELEM_0(MINI_PP_MAKE_TUPLE(__VA_ARGS__))) REPEAT_IMPL_5 MINI_PP_TUPLE_POP_FRONT(MINI_PP_MAKE_TUPLE(__VA_ARGS__))
+#define REPEAT_IMPL_7( func, ...) MINI_PP_CALL_WITH_ARGS(func, MINI_PP_TUPLE_GET_ELEM_0(MINI_PP_MAKE_TUPLE(__VA_ARGS__))) REPEAT_IMPL_6 MINI_PP_TUPLE_POP_FRONT(MINI_PP_MAKE_TUPLE(__VA_ARGS__))
+
+enum class EnumDoForEach
+{
+    First = 0,
+
+    #define DECLARE_FIELD(arg, x) MINI_PP_CAT(Field, x),
+    #define DECLARE_FIELDS(...) FOR_EACH_WITH_ARGS(DECLARE_FIELD, dummy, __VA_ARGS__)
+
+    //DECLARE_FIELDS()
+    DECLARE_FIELDS(A, B, C, D)
+
+    //DO_FOR_EACH(DECLARE_FIELD, E, F, G, H)
+};
+
+static_assert(static_cast<int>(EnumDoForEach::FieldB) == 2, "'DO_FOR_EACH' Broken");
+static_assert(static_cast<int>(EnumDoForEach::FieldD) == 4, "'DO_FOR_EACH' Broken");
+static_assert(static_cast<int>(EnumDoForEach::FieldH) == 8, "'DO_FOR_EACH' Broken");
